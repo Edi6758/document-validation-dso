819c154f04547c96bdaf506d272cffa34e8de22a
GusGado <gujjbrito@gmail.com>
2022-06-15 12:59:58 -0300

    we changed the location of the document register to the document controller and made notes afterwards on what to change after the meeting with the teacher

--- controles/ControladorDocumento.py
+++ controles/ControladorDocumento.py
@@ -5,17 +5,22 @@ from telas.TelaDocumento import TelaDocumento
 
 class ControladorDocumento:
     def __init__(self):
-        self.__cpf = []
+        self.__cpfs = []
         self.__rg = []
         self.__titulo_eleitor = []
         self.__teladocumento = TelaDocumento()
 
     #VERIFICAR COMO ESTÁ O CADASTRO DE DOCUMENTO
     def cadastro_documento(self):
-        documentos_cadastrados = self.__teladocumento()
-        cpf = Cpf(documentos_cadastrados['cpf'])
-        rg = Identidade(documentos_cadastrados['rg'])
-        titulo_eleitor = TituloEleitor(documentos_cadastrados['titulo_eleitor'])
+        # documentos_cadastrados = self.__teladocumento()
+        # cpf = Cpf(documentos_cadastrados['cpf'])
+        # rg = Identidade(documentos_cadastrados['rg'])
+        # titulo_eleitor = TituloEleitor(documentos_cadastrados['titulo_eleitor'])
+        #
+        # print(cpf, rg, titulo_eleitor)
+        dados_cpf = self.__teladocumento.cadastrar_cpf()
+        cpfx = Cpf(dados_cpf['cpf'], dados_cpf['nome'])
+        self.__cpfs.append(cpfx)
+        print(self.__cpfs)
 
-        print(cpf, rg, titulo_eleitor)
     #############################################
