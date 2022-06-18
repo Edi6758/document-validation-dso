d555dca09f489e4ad49b10292fde763e257e9dce
Zibet77 <zhivko.zhelev@diginius.com>
2022-06-18 10:57:45 -0300

    Fixing the prints, creating document menu, removing unnecessary returns

--- telas/TelaSistema.py
+++ telas/TelaSistema.py
@@ -2,29 +2,28 @@
 
 class TelaSistema:
     def mostra_opcoes(self):
-        print("---------MENU-----------")
-        print("Escolha a opcao")
-        print("1 - Cadastrar Usuario")
-        print("2 - Logar como Usuario")
-        print("0 - finalizar")
-        opcoes_menu_cadastro_ou_login = int(input("escolha uma opcao:"))
+        print("---------MENU-----------"
+              "\nEscolha a opcao"
+              "\n1 - Cadastrar Usuario"
+              "\n2 - Logar como Usuario"
+              "\n0 - Finalizar")
+        opcoes_menu_cadastro_ou_login = int(input("Escolha uma opcao: "))
         return opcoes_menu_cadastro_ou_login
 
     def mostrar_opcoes_apos_login(self):
-        print('------ BEM VINDO ------')
-
-        print('1 - Alterar ou listar Dados')
-        print('2 - validar documentos')
-        print('0 - retornar')
-        opcoes_menu_dados_ou_documentos = int(input('digite sua opção'))
+        print('------ BEM VINDO ------'
+              '\n1 - Alterar ou listar Dados'
+              '\n2 - validar documentos'
+              '\n0 - Fechar o programa')
+        opcoes_menu_dados_ou_documentos = int(input('Digite sua opção: '))
         return opcoes_menu_dados_ou_documentos
 
     def mostrar_opcoes_dados(self):
-        print('------ DADOS ------')
-
-        print('1 - listar usuarios cadastrados')
-        print('2 - alterar dados')
-        print('3 - excluir conta')
-        print('0 - retornar')
+        print('------ DADOS ------'
+              '\n1 - Listar usuarios cadastrados'
+              '\n2 - Alterar dados'
+              '\n2 - Alterar dados'
+              '\n3 - Excluir conta'
+              '\n0 - Retornar')
         opcoes_menu_dados = int(input('digite sua opção'))
         return opcoes_menu_dados
